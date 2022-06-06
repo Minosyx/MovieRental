5966d947c77ea9e5dfc1c606f73ae6569d0b793f
Patryk Tajs <57626171+Minosyx@users.noreply.github.com>
2022-06-06 02:28:59 +0200

    Add files via upload

---
+++ WypozyczalnieFilmowMVC/App_Start/IdentityConfig.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Entities;
+using Entities.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Twilio;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using WypozyczalnieFilmowMVC.Models;
+
+namespace WypozyczalnieFilmowMVC
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            await SendGridAsync(message);
+        }
+
+        private async Task SendGridAsync(IdentityMessage message)
+        {
+            var apiKey = ConfigurationManager.AppSettings["SendGridApi"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("minosyx@gmail.com", "Patryk T.");
+            var to = new EmailAddress(message.Destination);
+            var subject = message.Subject;
+            var plainTextContent = message.Body;
+            var htmlContent = message.Body;
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            var accountSid = ConfigurationManager.AppSettings["SMSAccountIdentification"];
+            var authToken = ConfigurationManager.AppSettings["SMSAccountPassword"];
+            var fromNumber = ConfigurationManager.AppSettings["SMSAccountFrom"];
+
+            TwilioClient.Init(accountSid, authToken);
+            var result = MessageResource.Create(
+                new PhoneNumber(message.Destination),
+                from: new PhoneNumber(fromNumber),
+                body: message.Body
+                );
+            Trace.TraceInformation(result.Status.ToString());
+            return Task.FromResult(0);
+        }
+    }
+
+    // Skonfiguruj menedżera użytkowników aplikacji używanego w tej aplikacji. Interfejs UserManager jest zdefiniowany w produkcie ASP.NET Identity i jest używany przez aplikację.
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<AppDbContext>()));
+            // Konfiguruj logikę weryfikacji nazw użytkowników
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Konfiguruj logikę weryfikacji haseł
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // Konfiguruj ustawienia domyślne blokady użytkownika
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            // Zarejestruj dostawców uwierzytelniania dwuetapowego. W przypadku tej aplikacji kod weryfikujący użytkownika jest uzyskiwany przez telefon i pocztą e-mail
+            // Możesz zapisać własnego dostawcę i dołączyć go tutaj.
+            manager.RegisterTwoFactorProvider("Code — phone", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "Your security code: {0}"
+            });
+            manager.RegisterTwoFactorProvider("Code — e-mail", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "Security code",
+                BodyFormat = "Your security code: {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = 
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Skonfiguruj menedżera logowania aplikacji używanego w tej aplikacji.
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+}
