818732038f61fdbfd0d55e94dafa9da738195b67
Patryk Tajs <57626171+Minosyx@users.noreply.github.com>
2022-06-06 02:31:05 +0200

    Add files via upload

---
+++ WypozyczalnieFilmowMVC/DefaultUserRoles.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Entities;
+using Entities.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WypozyczalnieFilmowMVC
+{
+    public static class DefaultUserRoles
+    {
+        public static async Task CreateRolesAndUsers()
+        {
+            AppDbContext _context = AppDbContext.Create();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+
+            string adminRole = "Admin";
+
+            if (!roleManager.RoleExists(adminRole))
+            {
+                var role = new IdentityRole
+                {
+                    Name = adminRole
+                };
+                await roleManager.CreateAsync(role);
+            }
+
+            if (await userManager.FindByNameAsync("admin123") == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "admin123",
+                    Email = ConfigurationManager.AppSettings["AdminMail"],
+                    EmailConfirmed = true
+                };
+
+                string pwd = ConfigurationManager.AppSettings["AdminPwd"];
+
+                var result = await userManager.CreateAsync(user, pwd);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user.Id, adminRole);
+                }
+            }
+
+            string employeeRole = "Employee";
+
+            if (!roleManager.RoleExists(employeeRole))
+            {
+                var role = new IdentityRole
+                {
+                    Name = employeeRole
+                };
+                await roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
---
+++ WypozyczalnieFilmowMVC/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using static WypozyczalnieFilmowMVC.DefaultUserRoles;
+
+[assembly: OwinStartupAttribute(typeof(WypozyczalnieFilmowMVC.Startup))]
+namespace WypozyczalnieFilmowMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesAndUsers().Wait();
+        }
+    }
+}
